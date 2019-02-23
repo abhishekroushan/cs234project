41382f9ff57dea4c41f0a62e9e598cb62b3f1237
Abhishek Roushan <aroushan@BMIR-C02P42AEG3QP.local>
2019-02-22 20:22:49 -0800

    generate trajectory fn added

--- mrp.py
+++ mrp.py
@@ -23,31 +23,48 @@ class MRP():
         if self.isEnd(state): return (state,0)
         next_state=self.next_state(state)
         if state%2==0:#even state
-            return (next_state,np.random.randn())
+            return (state,np.random.randn())
         else:#odd state
-            return (next_state,0.1)
+            return (state,0.1)
 
 #function for td lambda learning
-def td_learning(traj_list, num_states, td_update_fn, lambd, gamma):
+def td_learning(traj_list, num_states, td_update_fn, lambd, gamma, alpha):
     #initializations
     Vs      =np.zeros(num_states)
-    Es      =np.zeros(num_states)
     gamma_s =np.ones(num_states)
     lambd_s =np.ones(num_states)
 
     #update V, E, lambd, gamma for all states from the td_update_fn
     for episode in traj_list:
-        Vs, Es, lambd_s, gamma_s=td_update_fn(episode,Vs, Es, lambd_s, gamma_s)
-    return Vs, Es, lambd_s, gamma_s
+        Vs, lambd_s, gamma_s=td_update_fn(episode,Vs, lambd_s, gamma_s, alpha)
+    return Vs, lambd_s, gamma_s
 
 
+def generate_trajectories(problem, n_states, n_trajectories):
+    traj_list=[]
+    for n in range(n_trajectories):
+        s= problem.startState()
+        h=[]
+        while s!=n_states:
+            s_r=problem.get_successor_state_reward(s)
+            #next state
+            s=problem.next_state(s)
+            h.append(s_r)
+        traj_list.append(h)
+    return traj_list
+
+n_traj= 50
 num_states=10
 problem=MRP(num_states)
+traj_list=generate_trajectories(problem, num_states, n_traj)
+print("------------trajectories list------------")
+for item in traj_list:
+    print(item)
 s=problem.startState()
 history=[]
 while s!=num_states:
     state_reward=problem.get_successor_state_reward(s)
-    s=state_reward[0]
+    s=problem.next_state(s)
     history.append(state_reward)
 
 print("------------history------------------")
